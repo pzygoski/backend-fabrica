f492c057abe6ef88e0c93e4c95802d01bef83af0
pzygoski <pzygoski@gmail.com>
2025-04-13 11:33:11 -0400

    commit

--- servico/listarPedidos.js
+++ servico/listarPedidos.js
@@ -52,7 +52,7 @@ export async function listarPedidosAdmin(req, res) {
                 JOIN pedido_ingredientes pi ON p.id_pedido = pi.id_pedido
                 JOIN ingredientes i ON pi.id_ingrediente = i.id_ingrediente
                 LEFT JOIN enderecos e ON p.id_cliente = e.id_cliente
-                WHERE p.status = 'finalizado' AND p.data_criacao >= NOW() - INTERVAL 1 DAY
+                WHERE p.status = 'finalizado'
                 GROUP BY p.id_pedido, e.rua, e.numero, e.bairro, e.cep, e.complemento
             `;
         } else {
@@ -60,7 +60,24 @@ export async function listarPedidosAdmin(req, res) {
         }
 
         const [rows] = await pool.query(query);
-        res.json(rows);
+        const pedidosFormatados = rows.map(pedido => {
+            const data = new Date(pedido.data_criacao);
+            const dataFormatada = data.toLocaleString('pt-BR', {
+                day: '2-digit',
+                month: '2-digit',
+                year: 'numeric',
+                hour: '2-digit',
+                minute: '2-digit'
+            });
+
+            return {
+                ...pedido,
+                data_criacao: dataFormatada
+            };
+        });
+
+        res.json(pedidosFormatados);
+
     } catch (error) {
         console.error('Erro ao buscar pedidos:', error);
         res.status(500).json({ mensagem: 'Erro ao buscar pedidos' });
