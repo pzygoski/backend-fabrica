a1ba46c8f25afa8269894f5b37239fec131a9365
pzygoski <pzygoski@gmail.com>
2025-04-19 16:35:27 -0400

    commit

--- servico/listarPedidos.js
+++ servico/listarPedidos.js
@@ -16,6 +16,7 @@ export async function listarPedidosAdmin(req, res) {
                 MAX(CASE WHEN i.tipo = 'recheio' THEN i.nome END) AS recheio,
                 MAX(CASE WHEN i.tipo = 'cobertura' THEN i.nome END) AS cobertura,
                 MAX(CASE WHEN i.tipo = 'cor_cobertura' THEN i.nome END) AS cor_cobertura,
+                SUM(pi.quantidade) AS quantidade,
                 e.rua, 
                 e.numero, 
                 e.bairro, 
@@ -26,40 +27,22 @@ export async function listarPedidosAdmin(req, res) {
             JOIN pedido_ingredientes pi ON p.id_pedido = pi.id_pedido
             JOIN ingredientes i ON pi.id_ingrediente = i.id_ingrediente
             LEFT JOIN enderecos e ON p.id_cliente = e.id_cliente
-            WHERE p.status = 'aguardando'
         `;
 
         if (filtro === 'finalizados') {
-            query = `
-                SELECT 
-                    p.id_pedido, 
-                    p.data_criacao, 
-                    c.email AS email_cliente, 
-                    p.valor_total, 
-                    p.forma_pagamento, 
-                    p.status,
-                    MAX(CASE WHEN i.tipo = 'tamanho' THEN i.nome END) AS tamanho,
-                    MAX(CASE WHEN i.tipo = 'recheio' THEN i.nome END) AS recheio,
-                    MAX(CASE WHEN i.tipo = 'cobertura' THEN i.nome END) AS cobertura,
-                    MAX(CASE WHEN i.tipo = 'cor_cobertura' THEN i.nome END) AS cor_cobertura,
-                    e.rua, 
-                    e.numero, 
-                    e.bairro, 
-                    e.cep, 
-                    e.complemento
-                FROM pedidos p
-                JOIN clientes c ON p.id_cliente = c.id_cliente
-                JOIN pedido_ingredientes pi ON p.id_pedido = pi.id_pedido
-                JOIN ingredientes i ON pi.id_ingrediente = i.id_ingrediente
-                LEFT JOIN enderecos e ON p.id_cliente = e.id_cliente
+            query += `
                 WHERE p.status = 'finalizado'
                 GROUP BY p.id_pedido, e.rua, e.numero, e.bairro, e.cep, e.complemento
             `;
         } else {
-            query += ` GROUP BY p.id_pedido, e.rua, e.numero, e.bairro, e.cep, e.complemento`;
+            query += `
+                WHERE p.status = 'aguardando'
+                GROUP BY p.id_pedido, e.rua, e.numero, e.bairro, e.cep, e.complemento
+            `;
         }
 
         const [rows] = await pool.query(query);
+
         const pedidosFormatados = rows.map(pedido => {
             const data = new Date(pedido.data_criacao);
             const dataFormatada = data.toLocaleString('pt-BR', {
